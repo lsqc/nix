777504e0b7b7b597dc567cbcfc5333c296833f57
lsqc <lsqc@nya.vodka>
2025-09-05 07:46:17 +0200

    cleanup

--- hosts/vm/cookie/default.nix
+++ hosts/vm/cookie/default.nix
@@ -1,27 +1,19 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
       ../../../common
 
       ./users.nix
     ];
 
-  # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  boot.loader.grub.device = "/dev/sda";
 
-  networking.hostName = "cookie"; # Define your hostname.
+  networking.hostName = "cookie"; 
 
   networking.firewall = {
     enable = true;
@@ -30,16 +22,8 @@
 
   time.timeZone = "Europe/Berlin";
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     tree
-  #   ];
-  # };
   environment.systemPackages = with pkgs; [
-    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    neovim 
     wget
     curl
     git 
@@ -48,6 +32,5 @@
   ];
 
   system.stateVersion = "25.05"; 
-
 }
 
