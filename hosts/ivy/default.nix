6f80e94eada220262e8e1f72af721e4ec998addf
lsqc <lsqc@nya.vodka>
2025-09-10 00:11:13 +0200

    ivy: update whitelist

--- hosts/ivy/default.nix
+++ hosts/ivy/default.nix
@@ -58,10 +58,14 @@
 
     package = pkgs.papermc;
 
+    whitelist = {
+      lsqc = "c32bb0f4-eda0-477c-b16c-87390a7979b8";
+    };
+
     serverProperties = {
       max-players = 50;
       online-mode = false;
-
+      white-list = true;
     };
 
     jvmOpts = "-Xms8G -Xmx8G";
