b39eeb4a9bf6af9242ad1c92649e00d959d3533f
lsqc <lsqc@nya.vodka>
2025-09-12 00:33:11 +0200

    ivy: update structure

--- hosts/ivy/default.nix
+++ hosts/ivy/default.nix
@@ -4,6 +4,8 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./minecraft-server.nix
+
       ../../common
     ];
 
@@ -47,28 +49,6 @@
         autoindex_exact_size off;
       '';
     };
-  };
-
-  services.minecraft-server = {
-    enable = true;
-    eula = true; 
-    openFirewall = true;
-    declarative = true;
-    dataDir = "/mnt/nya/minecraft";
-
-    package = pkgs.papermc;
-
-    whitelist = {
-      lsqc = "c32bb0f4-eda0-477c-b16c-87390a7979b8";
-    };
-
-    serverProperties = {
-      max-players = 50;
-      online-mode = false;
-      white-list = true;
-    };
-
-    jvmOpts = "-Xms8G -Xmx8G";
   };
 
   system.stateVersion = "25.05"; 
