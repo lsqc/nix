41efb6ce962ac5c70633e7e5cd775bcedeac7bba
lsqc <lsqc@nya.vodka>
2025-10-06 08:23:30 +0200

    add forgejo runner lxc

---
+++ hosts/lxc/runner-2/default.nix
@@ -0,0 +1,36 @@
+{ config, modulesPath, pkgs, lib, ...}:
+
+{
+  imports = 
+    [ 
+      (modulesPath + "/virtualisation/proxmox-lxc.nix")
+
+      ../../../common
+    ];
+
+  nix.settings = { sandbox = false; };
+  proxmoxLXC = {
+    manageNetwork = false;
+    privileged = false;
+  };
+
+  services.gitea-actions-runner = {
+    package = pkgs.forgejo-actions-runner;
+    instances.default = {
+      enable = true;
+      name = "monolith";
+      url = "https://git.nya.vodka";
+      tokenFile = config.age.secrets.forgejo-runner-token.path;
+      labels = [
+        "ubuntu-latest:docker://node:16-bullseye"
+        "ubuntu-22.04:docker://node:16-bullseye"
+        "ubuntu-20.04:docker://node:16-bullseye"
+        "ubuntu-18.04:docker://node:16-buster"     
+      ];
+    };
+  };
+
+  # security.pam.services.sshd.allowNullPassword = true;
+  system.stateVersion = "25.05";
+}
+
