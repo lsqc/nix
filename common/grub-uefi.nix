f291c17f187d6fe76fbf31208d189ea3651d2b54
lsqc <lsqc@nya.vodka>
2025-10-21 00:27:27 +0200

    add common/grub-uefi.nix

---
+++ common/grub-uefi.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+
+{
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi";
+    };
+    grub = {
+       efiSupport = true;
+       device = "nodev";
+    };
+  };
+}
