fe82161014d588cc1837f393e2d534ff1e391f15
lsqc <lsqc@nya.vodka>
2025-10-26 01:16:12 +0200

    enable git

--- common/home/lsqc/programs.nix
+++ common/home/lsqc/programs.nix
@@ -21,6 +21,27 @@
       };
     };
 
+    git = {
+      enable = true;
+      userEmail = "lsqc@nya.vodka";
+      userName = "lsqc";
+
+      signing = {
+        format = "ssh";
+        signByDefault = true;
+
+        key = "~/.ssh/gitkey";
+      };
+      aliases = {
+        st = "status";
+        c = "commit";
+        me = "merge";
+        cp = "cherry-pick";
+        b = "branch";
+        co = "checkout";
+      };
+    };
+
     yazi = { enable = true; };
 
     # web browser
