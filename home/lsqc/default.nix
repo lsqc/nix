0297250d7b72448321e603cadc244215ca6feb96
lsqc <lsqc@nya.vodka>
2025-11-18 23:10:55 +0100

    update some home manager stuff

--- home/lsqc/default.nix
+++ home/lsqc/default.nix
@@ -30,5 +30,7 @@
 
     fuzzel
     swaylock
+
+    prismlauncher
   ];
 }
--- home/lsqc/modules/default.nix
+++ home/lsqc/modules/default.nix
@@ -1,6 +1,13 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ ./helix.nix ./git.nix ./zsh.nix ./tmux.nix ./fnott.nix ./swaylock.nix ];
+  imports = [
+    ./helix.nix
+    ./git.nix
+    ./rofi.nix
+    ./zsh.nix
+    ./tmux.nix
+    ./fnott.nix
+    ./swaylock.nix
+  ];
 }
