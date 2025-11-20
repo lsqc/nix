cbc62dc3597abde24e37a62d84b884e7fbd721ca
lsqc <lsqc@nya.vodka>
2025-11-20 11:48:02 +0100

    home-manager: update yazi module

--- home/lsqc/modules/yazi.nix
+++ home/lsqc/modules/yazi.nix
@@ -1,10 +1,28 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.yazi = {
     enable = true;
-    settings = {
-      # theme = "";
+
+    # https://github.com/stepbrobd/nord.yazi
+    plugins = with pkgs.yaziPlugins; { inherit nord yatline full-border; };
+
+    flavors = { inherit (pkgs.yaziPlugins) nord; };
+
+    theme.flavor = {
+      light = "nord";
+      dark = "nord";
     };
+
+    initLua = # lua
+      ''
+        require("yatline"):setup({
+          theme = require("nord"):setup(),
+        })
+        require("full-border"):setup {
+        	type = ui.Border.ROUNDED,
+        }
+      '';
+
   };
 }
