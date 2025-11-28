ee4a18cd1cf6b4815296dc480eaa34a3a8eb01b1
lsqc <lsqc@nya.vodka>
2025-11-28 10:25:42 +0100

    helix: add rust language server and formatter

--- home/lsqc/modules/helix.nix
+++ home/lsqc/modules/helix.nix
@@ -12,6 +12,11 @@
         line-number = "relative";
         lsp.display-messages = true;
 
+        inline-diagnostics = {
+          cursor-line = "hint";
+          other-lines = "hint";
+        };
+
         cursor-shape = {
           normal = "block";
           insert = "bar";
@@ -35,18 +40,37 @@
       };
     };
 
-    languages.language = [
-      {
-        name = "nix";
-        auto-format = true;
-        formatter.command = "${pkgs.nixfmt-classic}/bin/nixfmt";
-      }
-      {
-        name = "java";
-        auto-format = true;
-        formatter.command = "${pkgs.google-java-format}/bin/google-java-format";
-      }
-    ];
+    languages = {
+      language = [
+        {
+          name = "nix";
+          auto-format = true;
+          formatter.command = "${pkgs.nixfmt-classic}/bin/nixfmt";
+        }
+        {
+          name = "java";
+          auto-format = true;
+          formatter.command =
+            "${pkgs.google-java-format}/bin/google-java-format";
+        }
+
+        {
+          name = "rust";
+          auto-format = true;
+          formatter.command = "${pkgs.rustfmt}/bin/rustfmt";
+        }
+      ];
+
+      language-server = {
+        rust-analyzer = {
+          command = "${pkgs.rust-analyzer}/bin/rust-analyzer";
+          config = {
+            check = { command = "${pkgs.clippy}/bin/cargo-clippy"; };
+            cargo = { features = "all"; };
+          };
+        };
+      };
+    };
 
     themes = {
       catppuccin_frappe_transparent = {
