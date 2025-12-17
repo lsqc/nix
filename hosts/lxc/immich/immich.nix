8b2a64d7f848f918e9ab6ff1c73e25df92f7d289
lsqc <lsqc@nya.vodka>
2025-12-17 08:30:37 +0100

    update shit

--- hosts/lxc/immich/immich.nix
+++ hosts/lxc/immich/immich.nix
@@ -7,12 +7,12 @@
     host = "0.0.0.0";
     openFirewall = true;
 
-    database = {
-      enable = true;
-      port = 3306;
-      host = "mysql-1.proxima-centauri.lsdevcloud.net";
-      user = "immich";
-    };
+    # database = {
+    #   enable = true;
+    #   port = 3306;
+    #   host = "mysql-1.proxima-centauri.lsdevcloud.net";
+    #   user = "immich";
+    # };
 
     secretsFile = config.age.secrets.immich-db.path;
 
