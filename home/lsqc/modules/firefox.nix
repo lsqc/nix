79952b6b652b5cd2d40353e99351a9cfc1c45d71
lsqc <lsqc@nya.vodka>
2025-12-22 21:54:14 +0100

    add nix shit to firefox search engines

--- home/lsqc/modules/firefox.nix
+++ home/lsqc/modules/firefox.nix
@@ -62,6 +62,42 @@
         "browser.newtabpage.activity-stream.showSponsored" = false;
         "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
       };
+
+      search.engines = {
+        nix-packages = {
+          name = "Nix Packages";
+          urls = [{
+            template = "https://search.nixos.org/packages";
+            params = [
+              {
+                name = "type";
+                value = "packages";
+              }
+              {
+                name = "query";
+                value = "{searchTerms}";
+              }
+            ];
+          }];
+
+          icon =
+            "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+          definedAliases = [ "@np" ];
+        };
+
+        nixos-wiki = {
+          name = "NixOS Wiki";
+          urls = [{
+            template =
+              "https://wiki.nixos.org/w/index.php?search={searchTerms}";
+          }];
+          iconMapObj."16" = "https://wiki.nixos.org/favicon.ico";
+          definedAliases = [ "@nw" ];
+        };
+
+        bing.metaData.hidden = true;
+        google.metaData.alias = "@g";
+      };
     };
   };
 }
