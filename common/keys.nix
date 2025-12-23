78443cc38a575f5f77881271d97262942a9fc349
lsqc <lsqc@nya.vodka>
2025-12-23 21:51:13 +0100

    add yubi public key to common/keys.nix

--- common/keys.nix
+++ common/keys.nix
@@ -1,10 +1,12 @@
 {
-    lsqc = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMe1eur6B43u8IZWoSeW0dEqC1+3vX8lMkmRxm5yFCuG lsqc@lsdevcloud.net"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFibZMulO+0XmRoFi4r8rhYix3EHcmxzI9VZQaQmsOZf uwu@lsqc.de"
-    ];
+  lsqc = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMe1eur6B43u8IZWoSeW0dEqC1+3vX8lMkmRxm5yFCuG lsqc@lsdevcloud.net"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFibZMulO+0XmRoFi4r8rhYix3EHcmxzI9VZQaQmsOZf uwu@lsqc.de"
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIPqzM8AW9w03e+QbLdETos87bPjmra6gfDWRMV5IO+ALAAAABHNzaDo= lsqc-yubi"
+  ];
 
-    root = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMe1eur6B43u8IZWoSeW0dEqC1+3vX8lMkmRxm5yFCuG lsqc@lsdevcloud.net"
-    ];
+  root = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMe1eur6B43u8IZWoSeW0dEqC1+3vX8lMkmRxm5yFCuG lsqc@lsdevcloud.net"
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIPqzM8AW9w03e+QbLdETos87bPjmra6gfDWRMV5IO+ALAAAABHNzaDo= lsqc-yubi"
+  ];
 }
