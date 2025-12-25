6b5ba8a09e84242c5dbb5a891bfa5df56c78f338
lsqc <lsqc@nya.vodka>
2025-12-25 14:18:57 +0100

    configure firefox as default browser in xdg config

--- home/lsqc/modules/xdg.nix
+++ home/lsqc/modules/xdg.nix
@@ -4,14 +4,14 @@
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
-      "text/html" = "librewolf.desktop";
+      "text/html" = "firefox.desktop";
 
-      "application/pdf" = "librewolf.desktop";
+      "application/pdf" = "firfox.desktop";
 
-      "x-scheme-handler/http" = "librewolf.desktop";
-      "x-scheme-handler/https" = "librewolf.desktop";
-      "x-scheme-handler/about" = "librewolf.desktop";
-      "x-scheme-handler/unknown" = "librewolf.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/about" = "firefox.desktop";
+      "x-scheme-handler/unknown" = "firefox.desktop";
 
       "x-scheme-handler/mailto" = "thunderbird.desktop";
     };
