5a783d04e53f8a07804f6a481e2e4f726fa1b3f9
lsqc <lsqc@nya.vodka>
2025-12-30 11:30:13 +0100

    hyprlock

--- home/lsqc/modules/hyprlock.nix
+++ home/lsqc/modules/hyprlock.nix
@@ -4,18 +4,19 @@
   programs.hyprlock = {
     enable = true;
 
-    extraConfig = ''
-      label {
-          monitor =
-          text = cmd[update:1000] echo "<span>$(date +"%I:%M")</span>"
-          color = rgba(216, 222, 233, .75)
-          font_size = 40
-          font_family = SF Pro Display Bold
-          position = 0, 80
-          halign = center
-          valign = center
-      }
-    '';
+    # extraConfig = ''
+    #   label {
+    #       monitor =
+    #       text = cmd[update:1000] echo "<span>$(date +"%I:%M")</span>"
+    #       color = rgba(216, 222, 233, .75)
+    #       font_size = 40
+    #       font_family = SF Pro Display Bold
+    #       position = 0, 120
+    #       halign = center
+    #       valign = center
+    #   }
+
+    # '';
 
     settings = {
       general = {
@@ -28,16 +29,44 @@
         blur_size = 8;
       }];
 
+      label = [
+        # clock
+        {
+          # size = "200, 50";
+          font_size = 40;
+          font_family = "SF Pro Display Bold";
+          position = "0, 180";
+          monitor = "";
+          text = "<span>$TIME</span>";
+          font_color = "rgba(216, 222, 233, .75)";
+          #       color = rgba(216, 222, 233, .75)
+          # inner_color = "rgb(91, 96, 120)";
+          # outer_color = "rgb(24, 25, 38)";
+          shadow_passes = 2;
+        }
+        # user name
+        {
+          size = "200, 50";
+          position = "0, 50";
+          monitor = "";
+          text = "<span>$USER</span>";
+          font_color = "rgb(202, 211, 245)";
+          inner_color = "rgb(91, 96, 120)";
+          outer_color = "rgb(24, 25, 38)";
+          shadow_passes = 2;
+        }
+      ];
       input-field = [{
         size = "200, 50";
-        position = "0, 0";
+        position = "0, -20";
         monitor = "";
+        dots_size = 0.2;
         dots_center = true;
         fade_on_empty = false;
         # font_color = "rgb(202, 211, 245)";
         # inner_color = "rgb(91, 96, 120)";
         # outer_color = "rgb(24, 25, 38)";
-        outline_thickness = 5;
+        outline_thickness = 3;
         placeholder_text = "<span>Password...</span>";
         shadow_passes = 2;
       }];
