f9df30816c4518dc8d9a6b4080d462ed1575ce15
lsqc <lsqc@nya.vodka>
2026-01-01 23:32:25 +0100

    update forgejo config

--- hosts/vm/hachi/modules/forgejo.nix
+++ hosts/vm/hachi/modules/forgejo.nix
@@ -28,7 +28,7 @@ in {
         HTTP_PORT = 3000;
         SSH_PORT = lib.head config.services.openssh.ports;
       };
-      service.DISABLE_REGISTRATION = true;
+      service = { DISABLE_REGISTRATION = false; };
       actions = {
         ENABLED = true;
         DEFAULT_ACTIONS_URL = "github";
@@ -39,6 +39,7 @@ in {
         FROM = "noreply@${srv.DOMAIN}";
         USER = "noreply@${srv.DOMAIN}";
       };
+      migrations = { ALLOWED_DOMAINS = "*.nya.vodka"; };
     };
     secrets = {
       mailer.PASSWD = config.age.secrets.forgejo-mailer-password.path;
