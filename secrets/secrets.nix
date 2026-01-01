121af3a0a6754a92522a5391e47b4a6c79d38c1b
lsqc <lsqc@nya.vodka>
2026-01-01 22:49:30 +0100

    update secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   # users
-  lsqc =
-    "ssh-ed25519 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMe1eur6B43u8IZWoSeW0dEqC1+3vX8lMkmRxm5yFCuG";
+  lsqc = [
+    "ssh-ed25519 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMe1eur6B43u8IZWoSeW0dEqC1+3vX8lMkmRxm5yFCuG"
+  ];
 
   # systems
   immich =
@@ -10,8 +11,8 @@ let
   hachi =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOjLW6Lac1Fz+AF6SHYuomvAY3Z0333Yoi4HAy1Ra47J";
 in {
-  "immich-db.age".publicKeys = [ lsqc immich ];
-  "forgejo-mailer-password.age".publicKeys = [ lsqc hachi ];
+  "immich-db.age".publicKeys = lsqc ++ [ immich ];
+  "forgejo-mailer-password.age".publicKeys = lsqc ++ [ hachi ];
 
   "armored-secret.age" = {
     publicKeys = [ lsqc ];
