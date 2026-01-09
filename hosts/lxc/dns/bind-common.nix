09907a7f5c3ffe7ef4d204875859f97171bd007d
lsqc <lsqc@nya.vodka>
2026-01-09 01:04:13 +0100

    dnsssssssss

--- hosts/lxc/dns/bind-common.nix
+++ hosts/lxc/dns/bind-common.nix
@@ -4,10 +4,26 @@
   services.bind = {
     enable = true;
 
+    cacheNetworks = [ "127.0.0.0/8" "10.42.0.0/16" ];
     zones = { "pc.ip.nya.vodka" = { allowQuery = [ "10.0.0.0/8" ]; }; };
 
-    forward = "first";
+    forward = "only";
     forwarders = [ "10.42.0.2" "10.42.0.3" "10.42.0.4" ];
+
+    extraOptions = ''
+      recursion yes;
+      allow-recursion { any; };
+
+      # forward only;
+      # forwarders {
+      #   1.1.1.1;
+      #   9.9.9.9;
+      # };
+
+      dnssec-validation auto;
+      listen-on { any; };
+      listen-on-v6 { any; };
+    '';
   };
 
   networking.firewall = {
