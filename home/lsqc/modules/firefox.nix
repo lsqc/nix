dc7e1f189ef41d3b501fdf9f4fc43e3c2eb9a151
lsqc <lsqc@nya.vodka>
2026-01-14 20:40:46 +0100

    firefox

--- home/lsqc/modules/firefox.nix
+++ home/lsqc/modules/firefox.nix
@@ -117,81 +117,86 @@
 
       userChrome = builtins.readFile ../../../assets/firefox-theme.css;
 
-      search.engines = {
-        nix-packages = {
-          name = "Nix Packages";
-          urls = [{
-            template = "https://search.nixos.org/packages";
-            params = [
-              {
-                name = "type";
-                value = "packages";
-              }
-              {
-                name = "query";
-                value = "{searchTerms}";
-              }
-            ];
-          }];
+      search = {
+        force = true;
 
-          icon =
-            "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          definedAliases = [ "@np" ];
-        };
-
-        nix-options = {
-          name = "Nix Options";
-          urls = [{
-            template = "https://search.nixos.org/options";
-            params = [
-              {
-                name = "type";
-                value = "options";
-              }
-              {
-                name = "query";
-                value = "{searchTerms}";
-              }
-            ];
-          }];
-
-          icon =
-            "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          definedAliases = [ "@no" ];
-        };
-
-        nixos-wiki = {
-          name = "NixOS Wiki";
-          urls = [{
-            template =
-              "https://wiki.nixos.org/w/index.php?search={searchTerms}";
-          }];
-          iconMapObj."16" = "https://wiki.nixos.org/favicon.ico";
-          definedAliases = [ "@nw" ];
-        };
-
-        nix-dev = {
-          urls = [{
-            template = "https://nix.dev/search.html";
-            params = [{
-              name = "q";
-              value = "{searchTerms}";
+        engines = {
+          nix-packages = {
+            name = "Nix Packages";
+            urls = [{
+              template = "https://search.nixos.org/packages";
+              params = [
+                {
+                  name = "type";
+                  value = "packages";
+                }
+                {
+                  name = "query";
+                  value = "{searchTerms}";
+                }
+              ];
             }];
-          }];
-          icon =
-            "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          definedAliases = [ "@nd" ];
-        };
 
-        yt = {
-          name = "YouTube";
-          urls = [{ template = "https://youtube.com/search?q={searchTerms}"; }];
-          # iconMapObj."16" = "https://wiki.nixos.org/favicon.ico";
-          iconUpdateUrl = "https://youtube.com/favicon.ico";
-          definedAliases = [ "@yt" ];
-        };
+            icon =
+              "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@np" ];
+          };
 
-        google.metaData.alias = "@g";
+          nix-options = {
+            name = "Nix Options";
+            urls = [{
+              template = "https://search.nixos.org/options";
+              params = [
+                {
+                  name = "type";
+                  value = "options";
+                }
+                {
+                  name = "query";
+                  value = "{searchTerms}";
+                }
+              ];
+            }];
+
+            icon =
+              "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@no" ];
+          };
+
+          nixos-wiki = {
+            name = "NixOS Wiki";
+            urls = [{
+              template =
+                "https://wiki.nixos.org/w/index.php?search={searchTerms}";
+            }];
+            iconMapObj."16" = "https://wiki.nixos.org/favicon.ico";
+            definedAliases = [ "@nw" ];
+          };
+
+          nix-dev = {
+            urls = [{
+              template = "https://nix.dev/search.html";
+              params = [{
+                name = "q";
+                value = "{searchTerms}";
+              }];
+            }];
+            icon =
+              "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@nd" ];
+          };
+
+          yt = {
+            name = "YouTube";
+            urls =
+              [{ template = "https://youtube.com/search?q={searchTerms}"; }];
+            # iconMapObj."16" = "https://wiki.nixos.org/favicon.ico";
+            iconUpdateUrl = "https://youtube.com/favicon.ico";
+            definedAliases = [ "@yt" ];
+          };
+
+          google.metaData.alias = "@g";
+        };
       };
     };
   };
