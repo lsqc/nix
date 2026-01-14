02efcaa6a4955882062e00ab37d9e1a6705d0e9b
lsqc <lsqc@nya.vodka>
2026-01-14 13:31:04 +0100

    dns

--- hosts/lxc/dns/bind-common.nix
+++ hosts/lxc/dns/bind-common.nix
@@ -5,11 +5,35 @@
     enable = true;
 
     cacheNetworks = [ "127.0.0.0/8" "10.42.0.0/16" ];
-    zones = { "pc.ip.nya.vodka" = { allowQuery = [ "10.0.0.0/8" ]; }; };
+    # zones = {
+    #   "pc.ip.nya.vodka" = { allowQuery = [ "10.0.0.0/8" ]; };
+    #   "nya.vodka" = { allowQuery = [ "10.0.0.0/8" ]; };
+    # };
 
     forward = "only";
     forwarders = [ "10.42.0.2" "10.42.0.3" "10.42.0.4" ];
 
+    zones = {
+      "pc.ip.nya.vodka" = {
+        master = true;
+        slaves = [ "10.42.0.6" "10.42.0.7 " ];
+        file = let
+          zoneTemplate = ../../../dns/pc.ip.nya.vodka.zone;
+          serialTimestamp = lib.strings.trim (builtins.readFile
+            (pkgs.runCommand "zone-serial" { } ''
+              date +%s > $out
+            ''));
+
+          content = builtins.readFile zoneTemplate;
+          updatedZone =
+            builtins.replaceStrings [ "serial-number" ] [ serialTimestamp ]
+            content;
+
+          finalZone = updatedZone + "\n";
+
+        in pkgs.writeText "pc.ip.nya.vodka.zone" finalZone;
+      };
+    };
     extraOptions = ''
       recursion yes;
       allow-recursion { any; };
--- hosts/lxc/dns/dns1/bind.nix
+++ hosts/lxc/dns/dns1/bind.nix
@@ -1,29 +1,8 @@
 { lib, pkgs, ... }:
 
 {
-  services.bind = {
+  # services.bind = {
 
-    zones = {
-      "pc.ip.nya.vodka" = {
-        master = true;
-        slaves = [ "10.42.0.6" "10.42.0.7 " ];
-        file = let
-          zoneTemplate = ../../../../dns/pc.ip.nya.vodka.zone;
-          serialTimestamp = lib.strings.trim (builtins.readFile
-            (pkgs.runCommand "zone-serial" { } ''
-              date +%s > $out
-            ''));
-
-          content = builtins.readFile zoneTemplate;
-          updatedZone =
-            builtins.replaceStrings [ "serial-number" ] [ serialTimestamp ]
-            content;
-
-          finalZone = updatedZone + "\n";
-
-        in pkgs.writeText "pc.ip.nya.vodka.zone" finalZone;
-      };
-    };
-  };
+  # };
 }
 
