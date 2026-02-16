0beea5b7936995486b7ddeac032c8695f40b2421
lsqc <lsqc@nya.vodka>
2026-02-16 14:27:42 +0100

    zsh: update prompt

--- home/lsqc/modules/zsh.nix
+++ home/lsqc/modules/zsh.nix
@@ -42,7 +42,7 @@
 
     initContent = ''
       #      PROMPT="%F{147}%n%F{122}::%F{61}%m %F{255}∈ %F{69}%~%F{14}%f %F{166}λ %F{255}"
-            PROMPT="%B%F{47}%m%f%b %F{195}⊃%f %F{45}%~%f%F{57} λ %f"
+            PROMPT=" %B%F{47}%F{45}%~%f%F{57} λ %f"
 
             bindkey "^[[1;5C" forward-word
             bindkey "^[[1;5D" backward-word
