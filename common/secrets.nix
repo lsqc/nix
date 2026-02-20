5506b141664217ee6dac9b4c0e09b901db9d1e1b
lsqc <lsqc@nya.vodka>
2026-02-20 07:41:51 +0100

    update common/secrets.nix

--- common/secrets.nix
+++ common/secrets.nix
@@ -1,5 +1,10 @@
 { ... }:
 
 {
-  age.secrets.immich-db.file = ../secrets/immich-db.age;
+  age.secrets = {
+    immich-db.file = ../secrets/immich-db.age;
+    postgres-immich-pw.file = ../secrets/postgres-immich-pw.age;
+    postgres-postgres-pw.file = ../secrets/postgres-postgres-pw.age;
+    forgejo-mailer-password.file = ../secrets/forgejo-mailer-password.age;
+  };
 }
