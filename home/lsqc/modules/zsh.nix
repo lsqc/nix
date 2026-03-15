56b8879533a0c1c25982bad841d936d47160ed94
lsqc <lsqc@nya.vodka>
2026-03-15 17:37:48 +0100

    zsh config

--- home/lsqc/modules/zsh.nix
+++ home/lsqc/modules/zsh.nix
@@ -49,7 +49,7 @@
             bindkey "^[[1;5C" forward-word
             bindkey "^[[1;5D" backward-word
             bindkey "^[[3~" delete-char
-
+            bindkey "^[[3;5~" kill-word
     '';
   };
 }
