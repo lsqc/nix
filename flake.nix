2a7887a68b1a00da48e130b2aa36548c2c056e11
lsqc <lsqc@nya.vodka>
2026-05-17 20:24:21 +0200

    switch niri branch (`main` -> `very-refactor`)

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     niri = {
-      url = "github:sodiboo/niri-flake";
+      url = "github:sodiboo/niri-flake/very-refactor";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
