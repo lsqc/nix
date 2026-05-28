606580cb105c4273fea2026f27b596cfeb93045e
lsqc <lsqc@nya.vodka>
2026-05-28 21:14:47 +0200

    zsh theming

--- home/lsqc/modules/zsh.nix
+++ home/lsqc/modules/zsh.nix
@@ -10,10 +10,45 @@
     syntaxHighlighting = {
       enable = true;
       styles = {
-        command = "fg=#3bff96";
-        alias = "fg=#7e008a,bold";
-        path = "fg=#0fdb6e,italic";
-        unknown-token = "fg=#999999";
+        command = "fg=#00d7af,bold";
+        alias = "fg=#af87ff,bold,italic";
+        path = "fg=#5fd7ff,italic";
+        unknown-token = "fg=#ffffff,bold,bg=#ff0000";
+
+        reserved-word = "fg=#ffff00,bold";
+        builtin = "fg=#00d7af,bold";
+        function = "fg=#00ff5f,bold";
+        precommand = "fg=#00d7af,underline";
+        hashed-command = "fg=#00d7af";
+        suffix-alias = "fg=#af87ff,bold";
+        global-alias = "fg=#af87ff,bold";
+        autodirectory = "fg=#5fd7ff,bold";
+
+        path_prefix = "fg=#5fd7ff";
+        path_pathseparator = "fg=#585858";
+
+        single-hyphen-option = "fg=#00ff5f,bold";
+        double-hyphen-option = "fg=#00ff5f,bold";
+        arg0 = "fg=#00d7af";
+
+        single-quoted-argument = "fg=#00ff5f";
+        double-quoted-argument = "fg=#00ff5f";
+        dollar-single-quoted-argument = "fg=#00ffff,bold";
+        dollar-double-quoted-argument = "fg=#00ffff,bold";
+        back-quoted-argument = "fg=#af87ff";
+        back-double-quoted-argument = "fg=#00ffff";
+        back-dollar-quoted-argument = "fg=#00ffff";
+
+        commandseparator = "fg=#ffff00";
+        redirection = "fg=#ffff00,bold";
+        named-fd = "fg=#af87ff";
+        numeric-fd = "fg=#af87ff";
+
+        globbing = "fg=#ffff00,bold";
+        history-expansion = "fg=#af87ff,bold";
+        assign = "none";
+        comment = "fg=#585858,italic";
+        default = "none";
       };
     };
 
